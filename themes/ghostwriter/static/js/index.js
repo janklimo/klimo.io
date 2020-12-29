41abda94811f7cbaa46afa3331480b0b72d5d5b8
Jan Klimo <jan.klimo@gmail.com>
2020-12-30 01:29:59 +0700

    Use chroma - Native Hugo highlighter

--- themes/ghostwriter/static/js/index.js
+++ themes/ghostwriter/static/js/index.js
@@ -1,17 +1,12 @@
 // Required by lightbox
 import $ from "jquery";
-import hljs from "./highlight.js";
 import "magnific-popup/dist/jquery.magnific-popup";
 
 import "normalize.css";
-import "highlight.js/styles/zenburn.css";
 
 // Ligthbox
 import "magnific-popup/dist/magnific-popup.css";
-
 import "../styles/index.scss";
-
-hljs.initHighlightingOnLoad();
 
 $(".app__thumbnail--link").magnificPopup({
   type: "image",
