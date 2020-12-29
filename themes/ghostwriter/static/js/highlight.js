b986547435a7cda98052b6935bff94bc39fc2a8f
Jan Klimo <jan.klimo@gmail.com>
2020-12-30 00:42:26 +0700

    Try a different way to import highlight.js

---
+++ themes/ghostwriter/static/js/highlight.js
@@ -0,0 +1,17 @@
+import hljs from "highlight.js/lib/core";
+
+import bash from "highlight.js/lib/languages/bash";
+import css from "highlight.js/lib/languages/css";
+import javascript from "highlight.js/lib/languages/javascript";
+import ruby from "highlight.js/lib/languages/ruby";
+import typescript from "highlight.js/lib/languages/typescript";
+import yaml from "highlight.js/lib/languages/yaml";
+
+hljs.registerLanguage("bash", bash);
+hljs.registerLanguage("css", css);
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("ruby", ruby);
+hljs.registerLanguage("typescript", typescript);
+hljs.registerLanguage("yaml", yaml);
+
+export default hljs;
--- themes/ghostwriter/static/js/index.js
+++ themes/ghostwriter/static/js/index.js
@@ -1,15 +1,7 @@
-import hljs from "highlight.js/lib/core";
-
 // Required by lightbox
 import $ from "jquery";
+import hljs from "./highlight.js";
 import "magnific-popup/dist/jquery.magnific-popup";
-
-import bash from "highlight.js/lib/languages/bash";
-import css from "highlight.js/lib/languages/css";
-import javascript from "highlight.js/lib/languages/javascript";
-import ruby from "highlight.js/lib/languages/ruby";
-import typescript from "highlight.js/lib/languages/typescript";
-import yaml from "highlight.js/lib/languages/yaml";
 
 import "normalize.css";
 import "highlight.js/styles/zenburn.css";
@@ -18,13 +10,6 @@ import "highlight.js/styles/zenburn.css";
 import "magnific-popup/dist/magnific-popup.css";
 
 import "../styles/index.scss";
-
-hljs.registerLanguage("bash", bash);
-hljs.registerLanguage("css", css);
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("ruby", ruby);
-hljs.registerLanguage("typescript", typescript);
-hljs.registerLanguage("yaml", yaml);
 
 hljs.initHighlightingOnLoad();
 
